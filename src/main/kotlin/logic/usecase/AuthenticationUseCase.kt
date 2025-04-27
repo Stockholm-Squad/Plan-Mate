802a4c03faf4b897f631ff838ba0449b1fac5e90
Mona <monaayman584@gmail.com>
2025-04-27 09:59:59 +0300

    remove duplicated class

--- src/main/kotlin/logic/usecase/AuthenticationUseCase.kt
+++
@@ -1,6 +0,0 @@
-package org.example.logic.usecase
-
-import org.example.logic.repository.AuthenticationRepository
-
-class AuthenticationUseCase(private val authenticationRepository: AuthenticationRepository) {
-}
