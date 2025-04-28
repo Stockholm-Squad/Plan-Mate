52b91e67876e2e5cfa7f8ce5d0c4f1afb55eb8eb
hamsa <hamssaalsaadie@gmail.com>
2025-04-28 12:03:52 +0300

    add AuditSystemManagerUi constructor

--- src/main/kotlin/ui/features/audit/AuditSystemManagerUi.kt
+++ src/main/kotlin/ui/features/audit/AuditSystemManagerUi.kt
@@ -1,8 +1,23 @@
 package org.example.ui.features.audit
 
+import org.example.input_output.input.InputReader
+import org.example.input_output.output.OutputPrinter
 import org.example.logic.usecase.audit.ManageAuditSystemUseCase
+import org.example.ui.PlanMateConsoleUi
 
 class AuditSystemManagerUi(
-    private val getAuditSystemUseCase: ManageAuditSystemUseCase
-) {
+    private val getAuditSystemUseCase: ManageAuditSystemUseCase,
+    private val printer: OutputPrinter,
+    private val reader: InputReader,
+    private val auditSystemPlayer: PlanMateConsoleUi = PlanMateConsoleUi(
+        manageAuthenticationUi = TODO(),
+        manageAuditSystemUi = TODO(),
+        manageProjectUi = TODO(),
+        stateManagerUi = TODO(),
+        taskManagerUi = TODO()
+    ),
+
+    ) {
+
+
 }
