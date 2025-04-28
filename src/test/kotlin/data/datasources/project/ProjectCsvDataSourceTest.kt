4f77934c8e47df6d008af7e87f2a58dc2a75dc34
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-04-28 19:44:50 +0300

    Update test cases to cover 100 %

--- src/test/kotlin/data/datasources/project/ProjectCsvDataSourceTest.kt
+++ src/test/kotlin/data/datasources/project/ProjectCsvDataSourceTest.kt
@@ -7,19 +7,25 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import utils.buildProject
+import utils.mockInvalidFile
 import java.io.File
+import java.io.FileNotFoundException
 import java.nio.file.Files
 
 class ProjectCsvDataSourceTest {
 
     private lateinit var tempFile: File
     private lateinit var projectCsvDataSource: ProjectCsvDataSource
-
+    private lateinit var invalidFile: File
+    private lateinit var projectCsvDataSourceWithInvalidFile: ProjectCsvDataSource
     @BeforeEach
     fun setUp() {
         // Create a temporary file for testing
         tempFile = Files.createTempFile("projects", ".csv").toFile()
         projectCsvDataSource = ProjectCsvDataSource(tempFile)
+
+        invalidFile = mockInvalidFile()
+        projectCsvDataSourceWithInvalidFile = ProjectCsvDataSource(invalidFile)
     }
 
     @AfterEach
@@ -28,7 +34,6 @@ class ProjectCsvDataSourceTest {
         tempFile.delete()
     }
 
-    // Tests for getAllProjects()
     @Test
     fun `getAllProjects() should return a list of projects when data is available`() {
         val project1 = buildProject(name = "1", stateId = "1")
@@ -112,7 +117,7 @@ class ProjectCsvDataSourceTest {
 
     }
 
-    // Tests for getProjectById()
+
     @Test
     fun `getProjectById() should return a project when the ID exists`() {
         // Given
@@ -134,10 +139,31 @@ class ProjectCsvDataSourceTest {
     }
 
     @Test
-    fun `getProjectById() should return failure when the ID does not exist`() {
+    fun `getProjectById() should return a Failure when the ID don't exists`() {
+        // Given
+        val project1 = buildProject(
+            name = "Project 1",
+            stateId = "state-4"
+        )
+        val project2 = buildProject()
+        tempFile.writeText(
+            """
+            ${project1.id},${project1.name},${project1.stateId}
+            """.trimIndent()
+        )
+
+        // When
+        val result = projectCsvDataSource.getProjectById(project2.id)
+
+        // Then
+        assertThrows<Throwable> { result.getOrThrow() } // TODO: custom exception
+    }
+
+    @Test
+    fun `getProjectById() should return failure when the ID does not exist in empty file`() {
         // Given
         val project = buildProject()
-        tempFile.writeText("") // Empty file
+        tempFile.writeText("")
 
         // When
         val result = projectCsvDataSource.getProjectById(project.id)
@@ -178,10 +204,10 @@ class ProjectCsvDataSourceTest {
     }
 
     @Test
-    fun `editProject() should return failure when the project does not exist`() {
+    fun `editProject() should return failure when the project does not exist in empty file`() {
         // Given
         val project = buildProject()
-        tempFile.writeText("") // Empty file
+        tempFile.writeText("")
 
         val updatedProject = buildProject(
             id = project.id,
@@ -196,7 +222,31 @@ class ProjectCsvDataSourceTest {
         assertThrows<Throwable> {result.getOrThrow()} // TODO: custom Exception
     }
 
-    // Tests for deleteProject()
+    @Test
+    fun `editProject() should return failure when the project does not exist and the file has another projects`() {
+        // Given
+        val project = buildProject(
+            name = "Updated Project",
+            stateId = "state-6"
+        )
+
+        tempFile.writeText(
+            """
+            ${project.id},${project.name},${project.stateId}
+            """.trimIndent()
+        )
+
+        val project2 = buildProject()
+
+
+        // When
+        val result = projectCsvDataSource.editProject(project2)
+
+        // Then
+        assertThrows<Throwable> {result.getOrThrow()} // TODO: custom Exception
+    }
+
+
     @Test
     fun `deleteProject() should successfully delete an existing project`() {
         // Given
@@ -221,13 +271,230 @@ class ProjectCsvDataSourceTest {
     }
 
     @Test
-    fun `deleteProject() should return failure when the project does not exist`() {
+    fun `deleteProject() should return failure when the project does not exist and the file contain another projects`() {
+        // Given
+        val project = buildProject(
+            name = "project 22",
+            stateId = "state 333"
+        )
+        tempFile.writeText(
+            """
+            ${project.id},${project.name},${project.stateId}
+            """.trimIndent()
+        )
+        val project2 = buildProject()
+
+        // When
+        val result = projectCsvDataSource.deleteProject(project2.id)
+
+        // Then
+        assertThrows<Throwable> { result.getOrThrow() } // TODO: custom exception
+    }
+
+    @Test
+    fun `deleteProject() should return failure when the project does not exist in empty file`() {
         // Given
         val project = buildProject()
-        tempFile.writeText("") // Empty file
+        tempFile.writeText("")
 
         // When
         val result = projectCsvDataSource.deleteProject(project.id)
+
+        // Then
+        assertThrows<Throwable> { result.getOrThrow() } // TODO: custom exception
+    }
+
+    @Test
+    fun `getProjectById() should return failure when the file does not exist`() {
+        // Given
+        tempFile.delete() // Delete the file to simulate a missing file
+        val project = buildProject()
+
+        // When
+        val result = projectCsvDataSource.getProjectById(project.id)
+
+        // Then
+        assertThrows<Throwable> {assertThat(result.getOrThrow())} // TODO: custom exception
+    }
+
+    @Test
+    fun `getProjectById() should handle invalid CSV format`() {
+        // Given
+        tempFile.writeText(
+            """
+            invalid-line
+            """.trimIndent()
+        )
+        val project = buildProject()
+
+        // When
+        val result = projectCsvDataSource.getProjectById(project.id)
+
+        // Then
+        assertThrows<Throwable> {result.getOrThrow()} // TODO: custom exception
+    }
+
+    // Test for file existence check in addProject
+    @Test
+    fun `addProject() should return failure when the file does not exist`() {
+        // Given
+        tempFile.delete() // Delete the file to simulate a missing file
+        val project = buildProject()
+
+        // When
+        val result = projectCsvDataSource.addProject(project)
+
+        // Then
+        assertThat(result.isFailure).isTrue()
+        result.onFailure { cause ->
+            assertThat(cause).isInstanceOf(FileNotFoundException::class.java)
+            assertThat(cause.message).isEqualTo("File does not exist: ${tempFile.absolutePath}")
+        }
+    }
+
+    // Test for file existence check in editProject
+    @Test
+    fun `editProject() should return failure when the file does not exist`() {
+        // Given
+        tempFile.delete() // Delete the file to simulate a missing file
+        val project = buildProject()
+
+        // When
+        val result = projectCsvDataSource.editProject(project)
+
+        // Then
+        assertThrows<Throwable> { result.getOrThrow()} // TODO: custom exception
+    }
+
+    @Test
+    fun `editProject() should handle invalid CSV format`() {
+        // Given
+        tempFile.writeText(
+            """
+            invalid-line
+            """.trimIndent()
+        )
+        val project = buildProject()
+
+        // When
+        val result = projectCsvDataSource.editProject(project)
+
+        // Then
+        assertThrows<Throwable> { result.getOrThrow()} // TODO: custom exception
+    }
+
+    @Test
+    fun `deleteProject() should return failure when the file does not exist`() {
+        // Given
+        tempFile.delete()
+        val project = buildProject()
+
+        // When
+        val result = projectCsvDataSource.deleteProject(project.id)
+
+        // Then
+        assertThrows<Throwable> { result.getOrThrow()} // TODO: custom exception
+    }
+
+    @Test
+    fun `deleteProject() should handle invalid CSV format`() {
+        // Given
+        tempFile.writeText(
+            """
+            invalid-line
+            """.trimIndent()
+        )
+        val project = buildProject()
+
+        // When
+        val result = projectCsvDataSource.deleteProject(project.id)
+
+        // Then
+        assertThrows<Throwable> { result.getOrThrow()} // TODO: custom exception
+    }
+
+    @Test
+    fun `getAllProjects() should return failure when the file does not exist`() {
+        // Given
+        tempFile.delete()
+
+        // When
+        val result = projectCsvDataSource.getAllProjects()
+
+        // Then
+        assertThrows<Throwable> { result.getOrThrow()} // TODO: custom exception
+    }
+
+    @Test
+    fun `getAllProjects() should handle invalid CSV format`() {
+        // Given
+        tempFile.writeText(
+            """
+            invalid-line
+            """.trimIndent()
+        )
+
+        // When
+        val result = projectCsvDataSource.getAllProjects()
+
+        // Then
+        assertThat(result.getOrThrow()).isEmpty()
+    }
+
+    @Test
+    fun `getProjectById() should catch Throwable exceptions when Invalid File`() {
+        // Given
+        val project = buildProject()
+
+        // When
+        val result = projectCsvDataSourceWithInvalidFile.getProjectById(project.id)
+
+        // Then
+        assertThrows<Throwable> { result.getOrThrow() } // TODO: custom exception
+    }
+
+    @Test
+    fun `addProject() should catch Throwable exceptions when Invalid File`() {
+        // Given
+        val project = buildProject()
+
+        // When
+        val result = projectCsvDataSourceWithInvalidFile.addProject(project)
+
+        // Then
+        assertThrows<Throwable> { result.getOrThrow() } // TODO: custom exception
+    }
+
+    @Test
+    fun `editProject() should catch Throwable exceptions when Invalid File`() {
+        // Given
+        val project = buildProject()
+
+        // When
+        val result = projectCsvDataSourceWithInvalidFile.editProject(project)
+
+        // Then
+        assertThrows<Throwable> { result.getOrThrow() } // TODO: custom exception
+    }
+
+    @Test
+    fun `deleteProject() should catch Throwable exceptions when Invalid File`() {
+        // Given
+        val project = buildProject()
+
+        // When
+        val result = projectCsvDataSourceWithInvalidFile.deleteProject(project.id)
+
+        // Then
+        assertThrows<Throwable> { result.getOrThrow() } // TODO: custom exception
+    }
+
+    @Test
+    fun `getAllProjects() should catch Throwable exceptions when Invalid File`() {
+        // Given
+
+        // When
+        val result = projectCsvDataSourceWithInvalidFile.getAllProjects()
 
         // Then
         assertThrows<Throwable> { result.getOrThrow() } // TODO: custom exception
