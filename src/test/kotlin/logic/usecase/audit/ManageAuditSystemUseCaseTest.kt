610e28ebec30a7272a2b2460e7ce656d25455c3c
hamsa <hamssaalsaadie@gmail.com>
2025-04-28 10:23:54 +0300

    add manage audit system use case test for manage audit system use case

---
+++ src/test/kotlin/logic/usecase/audit/ManageAuditSystemUseCaseTest.kt
@@ -0,0 +1,22 @@
+package logic.usecase.audit
+
+import io.mockk.mockk
+import org.example.logic.repository.AuditSystemRepository
+import org.example.logic.usecase.audit.ManageAuditSystemUseCase
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class ManageAuditSystemUseCaseTest{
+
+  private lateinit var manageAuditSystemUseCase: ManageAuditSystemUseCase
+  private lateinit var auditSystemRepository: AuditSystemRepository
+
+  @BeforeEach
+  fun setUp(){
+   auditSystemRepository = mockk(relaxed = true)
+   manageAuditSystemUseCase = ManageAuditSystemUseCase(auditSystemRepository)
+  }
+
+
+ }
