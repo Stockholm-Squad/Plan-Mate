4a06721ac047e1101b5b4235ff8ef1a8baaf4549
hamsa <hamssaalsaadie@gmail.com>
2025-04-29 12:38:00 +0300

    add more test case for audit system csv data source

--- src/test/kotlin/data/datasources/audit/AuditSystemCsvDataSourceTest.kt
+++ src/test/kotlin/data/datasources/audit/AuditSystemCsvDataSourceTest.kt
@@ -36,4 +36,105 @@ class AuditSystemCsvDataSourceTest{
   assertThat(result.isSuccess).isTrue()
   assertThat(result.getOrNull()).isTrue()
  }
+
+ @Test
+ fun `getAuditSystemById should return correct audit system if exists`() {
+
+  // Given
+  val auditSystem = createAuditSystem(
+   id = "1",
+   auditSystemType = AuditSystemType.TASK,
+   entityId = "123",
+   changeDescription = "change description",
+   changedBy = "Hamsa")
+
+  auditSystemCsvDataSource.addAuditSystem(auditSystem)
+
+  // When
+  val result = auditSystemCsvDataSource.getAuditSystemById("1")
+
+  // Then
+  assertThat(result.isSuccess).isTrue()
+  assertThat(result.getOrNull()?.id).isEqualTo("1")
+ }
+
+ @Test
+ fun `getAllAuditSystems should return all added audit systems`() {
+
+  // Given
+  val auditSystem = listOf(createAuditSystem(
+   id = "1",
+   auditSystemType = AuditSystemType.TASK,
+   entityId = "123",
+   changeDescription = "change description",
+   changedBy = "Hamsa"),
+   createAuditSystem(
+    id = "2",
+    auditSystemType = AuditSystemType.TASK,
+    entityId = "123",
+    changeDescription = "change description",
+    changedBy = "Hamsa"))
+
+  auditSystem.forEach { auditSystemCsvDataSource.addAuditSystem(it) }
+
+  // When
+  val result = auditSystemCsvDataSource.getAllAuditSystems()
+
+  // Then
+  assertThat(result.isSuccess).isTrue()
+  assertThat(result.getOrNull()).hasSize(2)
+ }
+
+ @Test
+ fun `getAllAuditSystemsByType should return only matching type`() {
+
+  // Given
+  val auditSystem = listOf(createAuditSystem(
+   id = "1",
+   auditSystemType = AuditSystemType.TASK,
+   entityId = "123",
+   changeDescription = "change description",
+   changedBy = "Hamsa"),
+   createAuditSystem(
+    id = "2",
+    auditSystemType = AuditSystemType.TASK,
+    entityId = "123",
+    changeDescription = "change description",
+    changedBy = "Hamsa"))
+  auditSystem.forEach { auditSystemCsvDataSource.addAuditSystem(it) }
+
+  // When
+  val result = auditSystemCsvDataSource.getAllAuditSystemsByType(AuditSystemType.TASK)
+
+  // Then
+  assertThat(result.isSuccess).isTrue()
+  assertThat(result.getOrNull()).hasSize(2)
+  assertThat(result.getOrNull()?.first()?.auditSystemType).isEqualTo(AuditSystemType.TASK)
+ }
+
+ @Test
+ fun `getAllAuditSystemsEntityId should return only matching entity id`() {
+  // Given
+  val auditSystem = listOf(createAuditSystem(
+   id = "1",
+   auditSystemType = AuditSystemType.TASK,
+   entityId = "123",
+   changeDescription = "change description",
+   changedBy = "Hamsa"),
+   createAuditSystem(
+    id = "2",
+    auditSystemType = AuditSystemType.TASK,
+    entityId = "123",
+    changeDescription = "change description",
+    changedBy = "Hamsa"))
+  auditSystem.forEach { auditSystemCsvDataSource.addAuditSystem(it) }
+
+  // When
+  val result = auditSystemCsvDataSource.getAllAuditSystemsEntityId("1")
+
+  // Then
+  assertThat(result.isSuccess).isTrue()
+  assertThat(result.getOrNull()).hasSize(1)
+  assertThat(result.getOrNull()?.first()?.entityId).isEqualTo("1")
+ }
  }
