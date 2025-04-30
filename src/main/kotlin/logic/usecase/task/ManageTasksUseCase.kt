5af8672dae2fcdd2b691f5001ce82a5a9dcc5a89
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-05-01 01:44:41 +0300

    Update implementation of use case &test cases to  the task

--- src/main/kotlin/logic/usecase/task/ManageTasksUseCase.kt
+++ src/main/kotlin/logic/usecase/task/ManageTasksUseCase.kt
@@ -1,74 +1,106 @@
 package org.example.logic.usecase.task
 
 import logic.model.entities.Task
-import org.example.data.entities.MateTaskAssignment
-import org.example.data.entities.TaskInProject
+import org.example.logic.model.exceptions.PlanMateExceptions
 import org.example.logic.repository.TaskRepository
+import kotlin.Result
 import kotlin.String
+
 
 class ManageTasksUseCase(private val taskRepository: TaskRepository) {
 
     fun getAllTasks(): Result<List<Task>> =
         taskRepository.getAllTasks().fold(
             onSuccess = { Result.success(it) },
-            onFailure = { exception -> Result.failure(exception) }
+            onFailure = { Result.failure(PlanMateExceptions.LogicException.NoTasksFound()) }
         )
 
-    fun getTaskById(taskId: String?): Result<Task> =
+    fun getTaskById(taskId: String): Result<Task> =
         taskRepository.getAllTasks().fold(
             onSuccess = { tasks ->
                 tasks.find { it.id == taskId }
                     ?.let { Result.success(it) }
-                    ?: Result.failure(Throwable("Task with ID $taskId not found"))
+                    ?: Result.failure(PlanMateExceptions.LogicException.TaskNotFoundException())
             },
-            onFailure = { exception -> Result.failure(exception) }
+            onFailure = { Result.failure(PlanMateExceptions.LogicException.TaskNotFoundException()) }
         )
 
-    fun createTask(task: Task): Result<Boolean> {
-        return taskRepository.createTask(task).fold(
-            onSuccess = { Result.success(it) },
-            onFailure = { exception -> Result.failure(exception) }
+    fun createTask(task: Task): Result<Boolean> =
+         taskRepository.createTask(task).fold(
+            onSuccess = { Result.success(true) },
+            onFailure = { Result.failure(PlanMateExceptions.LogicException.NoTasksCreated()) }
         )
-    }
 
     fun editTask(updatedTask: Task): Result<Boolean> =
-        getTaskById(updatedTask.id).fold(
-            onSuccess = { existingTask ->
-                taskRepository.editTask(updatedTask)
-            },
-            onFailure = { exception ->
-                Result.failure(Throwable("Failed to find task with ID ${updatedTask.id}: ${exception.message}"))
-            }
-        )
-
-    fun deleteTask(taskId: String?): Result<Boolean> =
-        getTaskById(taskId).fold(
-            onSuccess = { existingTask ->
-                taskRepository.deleteTask(taskId)
-            },
-            onFailure = { exception ->
-                Result.failure(Throwable("Failed to find task with ID $taskId: ${exception.message}"))
-            }
-        )
-
-    fun getAllMateTaskAssignment(userName: String): Result<List<MateTaskAssignment>> =
-        taskRepository.getAllMateTaskAssignment(userName).fold(
+        taskRepository.editTask(updatedTask).fold(
             onSuccess = { Result.success(it) },
-            onFailure = { exception -> Result.failure(exception) }
+            onFailure = { Result.failure(PlanMateExceptions.LogicException.DidNotUpdateProject()) }
         )
 
+    fun deleteTask(taskId: String): Result<Boolean> =
+        getTaskById(taskId).fold(
+            onSuccess = {
+                taskRepository.deleteTask(taskId).fold(
+                    onSuccess = { Result.success(true) },
+                    onFailure = { Result.failure(PlanMateExceptions.LogicException.NoTasksDeleted()) }
+                )
+            },
+            onFailure = { Result.failure(PlanMateExceptions.LogicException.TaskNotFoundException()) }
+        )
+}
+
+
+
+
+//
 //    fun getAllTasksByProjectId(projectId: String): Result<List<TaskInProject>> =
-//        taskRepository.getAllTasksByProjectId(projectId).fold(
-//            onSuccess = { Result.success(it) },
-//            onFailure = { exception -> Result.failure(exception) }
+//        taskRepository.getAllTasksInProjects().fold(
+//            onSuccess = { links ->
+//                val filtered = links.filter { it.projectId == projectId }
+//                if (filtered.isEmpty()) Result.failure(PlanMateExceptions.LogicException.NoTasksFound())
+//                else Result.success(filtered)
+//            },
+//            onFailure = { Result.failure(PlanMateExceptions.LogicException.NoTasksFound()) }
 //        )
 //
 //    fun getAllStatesByProjectId(projectId: String): Result<List<Pair<String, String>>> =
-//        taskRepository.getAllTasksByProjectId(projectId).fold(
+//        getAllTasksByProjectId(projectId).fold(
 //            onSuccess = { tasks ->
 //                val states = tasks.map { Pair(it.taskId, it.stateId) }
 //                Result.success(states)
 //            },
-//            onFailure = { exception -> Result.failure(exception) }
+//            onFailure = { Result.failure(PlanMateExceptions.LogicException.NoTasksFound()) }
 //        )
-}
+//
+//    fun promptAndValidateState(projectId: String): Result<String?> {
+//        return getAllStatesByProjectId(projectId).fold(
+//            onSuccess = { states ->
+//                states
+//                    .map { it.second }
+//                    .firstOrNull()
+//                    .let { state ->
+//                        state?.let { Result.success(it) }
+//                            ?: Result.failure(PlanMateExceptions.LogicException.NoStatesFound())
+//                    }
+//            },
+//            onFailure = {
+//                Result.failure(PlanMateExceptions.LogicException.NoStatesFound())
+//            }
+//        )
+//    }
+//
+//    fun getAllProjects(): Result<List<TaskInProject>> =
+//        taskRepository.getAllTasksInProjects().fold(
+//            onSuccess = { Result.success(it.distinctBy { link -> link.projectId }) },
+//            onFailure = { Result.failure(PlanMateExceptions.LogicException.NoProjectAdded()) }
+//        )
+//
+//    fun getProjectIdByName(projectName: String): Result<String> =
+//        taskRepository.getAllTasksInProjects().fold(
+//            onSuccess = { projects ->
+//                projects.find { it.projectId == projectName }?.projectId
+//                    ?.let { Result.success(it) }
+//                    ?: Result.failure(PlanMateExceptions.LogicException.ProjectNotFoundException())
+//            },
+//            onFailure = { Result.failure(PlanMateExceptions.LogicException.ProjectNotFoundException()) }
+//        )
--- src/test/kotlin/logic/usecase/task/ManageTasksUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/task/ManageTasksUseCaseTest.kt
@@ -1,156 +1,240 @@
-//package logic.usecase.task
-//
-//import com.google.common.truth.Truth.assertThat
-//import org.example.logic.usecase.task.ManageTasksUseCase
-//import logic.model.entities.Task
-//import org.example.data.entities.MateTaskAssignment
-//import org.example.data.entities.TaskInProject
-//import org.example.logic.repository.TaskRepository
-//import io.mockk.every
-//import io.mockk.mockk
-//import kotlinx.datetime.LocalDateTime
-//import org.junit.jupiter.api.BeforeEach
-//import org.junit.jupiter.api.Test
-//import java.util.UUID
-//
-//class ManageTasksUseCaseTest {
-//
-//    private lateinit var taskRepository: TaskRepository
-//    private lateinit var manageTasksUseCase: ManageTasksUseCase
-//
-//    @BeforeEach
-//    fun setUp() {
-//        taskRepository = mockk(relaxed = true)
-//        manageTasksUseCase = ManageTasksUseCase(taskRepository)
-//    }
-//
-//    @Test
-//    fun `getAllTasks() should return success result with a list of tasks when the file has data`() {
-//        // Given
-//        val taskList = listOf(
-//            Task(id = "1", name = "Task 1", description = "Description 1", stateId = "state1", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0)),
-//            Task(id = "2", name = "Task 2", description = "Description 2", stateId = "state2", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0))
-//        )
-//        every { taskRepository.getAllTasks() } returns Result.success(taskList)
-//
-//        // When
-//        val result = manageTasksUseCase.getAllTasks()
-//
-//        // Then
-//        assertThat(result.getOrNull()).isEqualTo(taskList)
-//    }
-//
-//    @Test
-//    fun `getTaskById() should return success result with the task when the task is found`() {
-//        // Given
-//        val taskId = "1"
-//        val taskList = listOf(
-//            Task(id = "1", name = "Task 1", description = "Description 1", stateId = "state1", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0)),
-//            Task(id = "2", name = "Task 2", description = "Description 2", stateId = "state2", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0))
-//        )
-//        every { taskRepository.getAllTasks() } returns Result.success(taskList)
-//
-//        // When
-//        val result = manageTasksUseCase.getTaskById(taskId)
-//
-//        // Then
-//        assertThat(result.getOrNull()).isEqualTo(taskList.find { it.id == taskId })
-//    }
-//
-//    @Test
-//    fun `createTask() should return success result with true when the task is created successfully`() {
-//        // Given
-//        val newTask = Task(
-//            id = UUID.randomUUID().toString(),
-//            name = "Task 3",
-//            description = "Description 3",
-//            stateId = "state3",
-//            createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0),
-//            updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0)
-//        )
-//
-//        every { taskRepository.createTask(any()) } returns Result.success(true)
-//
-//        // When
-//        val result = manageTasksUseCase.createTask(newTask)
-//
-//        // Then
-//        assertThat(result.getOrNull()).isEqualTo(true)
-//    }
-//
-//    @Test
-//    fun `editTask() should return success result with true when the task is updated successfully`() {
-//        // Given
-//        val taskId = "1"
-//        val updatedTask = Task(
-//            id = taskId,
-//            name = "Updated Task",
-//            description = "Updated Description",
-//            stateId = "state2",
-//            createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0),
-//            updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0)
-//        )
-//        val existingTasks = listOf(
-//            Task(id = "1", name = "Task 1", description = "Description 1", stateId = "state1", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0))
-//        )
-//        every { taskRepository.getAllTasks() } returns Result.success(existingTasks)
-//        every { taskRepository.editTask(any()) } returns Result.success(true)
-//
-//        // When
-//        val result = manageTasksUseCase.editTask(updatedTask)
-//
-//        // Then
-//        assertThat(result.getOrNull()).isEqualTo(true)
-//    }
-//
-//    @Test
-//    fun `deleteTask() should return success result with true when the task is deleted successfully`() {
-//        // Given
-//        val taskId = "1"
-//        val existingTasks = listOf(
-//            Task(id = "1", name = "Task 1", description = "Description 1", stateId = "state1", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0))
-//        )
-//        every { taskRepository.getAllTasks() } returns Result.success(existingTasks)
-//        every { taskRepository.deleteTask(taskId) } returns Result.success(true)
-//
-//        // When
-//        val result = manageTasksUseCase.deleteTask(taskId)
-//
-//        // Then
-//        assertThat(result.getOrNull()).isEqualTo(true)
-//    }
-//
-//    @Test
-//    fun `getAllTasksByProjectId() should return success result with tasks filtered by project ID`() {
-//        // Given
-//        val projectId = "project1"
-//        val taskInProjectList = listOf(
-//            TaskInProject(taskId = "1", projectId = "project1"),
-//            TaskInProject(taskId = "2", projectId = "project2")
-//        )
-//        every { taskRepository.getAllTasksByProjectId(projectId) } returns Result.success(taskInProjectList)
-//
-//        // When
-//        val result = manageTasksUseCase.getAllTasksByProjectId(projectId)
-//
-//        // Then
-//        assertThat(result.getOrNull()).isEqualTo(listOf(TaskInProject(taskId = "1", projectId = "project1")))
-//    }
-//
-//    @Test
-//    fun `getAllTasksByUserId() should return success result with tasks filtered by user ID`() {
-//        // Given
-//        val userId = "user1"
-//        val userAssignedToTaskList = listOf(
-//            MateTaskAssignment(user = "user1", taskId = "1"),
-//            MateTaskAssignment(user = "user2", taskId = "2")
-//        )
-//        every { taskRepository.getAllMateTaskAssignment(userId) } returns Result.success(userAssignedToTaskList)
-//
-//        // When
-//        val result = manageTasksUseCase.getTaskById(userId)
-//
-//        // Then
-//        assertThat(result.getOrNull()).isEqualTo(listOf(MateTaskAssignment(user = "user1", taskId = "1")))
-//    }
-//}
+package logic.usecase.task
+
+import com.google.common.truth.Truth.assertThat
+import org.example.logic.usecase.task.ManageTasksUseCase
+import logic.model.entities.Task
+import org.example.data.entities.MateTaskAssignment
+import org.example.data.entities.TaskInProject
+import org.example.logic.repository.TaskRepository
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.datetime.LocalDateTime
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+class ManageTasksUseCaseTest {
+
+    private lateinit var taskRepository: TaskRepository
+    private lateinit var manageTasksUseCase: ManageTasksUseCase
+
+    @BeforeEach
+    fun setUp() {
+        taskRepository = mockk(relaxed = true)
+        manageTasksUseCase = ManageTasksUseCase(taskRepository)
+    }
+
+    @Test
+    fun `getAllTasks() should return success result with a list of tasks when the file has data`() {
+        // Given
+        val taskList = listOf(
+            Task(id = "1", name = "Task 1", description = "Description 1", stateId = "state1", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0)),
+            Task(id = "2", name = "Task 2", description = "Description 2", stateId = "state2", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0))
+        )
+        every { taskRepository.getAllTasks() } returns Result.success(taskList)
+
+        // When
+        val result = manageTasksUseCase.getAllTasks()
+
+        // Then
+        assertThat(result.getOrNull()).isEqualTo(taskList)
+    }
+
+    @Test
+    fun `getAllTasks() should return failure result when no tasks are found`() {
+        // Given
+        every { taskRepository.getAllTasks() } returns Result.success(emptyList<Task>())
+
+        // When
+        val result = manageTasksUseCase.getAllTasks()
+
+        // Then
+        assertThat(result.exceptionOrNull()?.message).isEqualTo("No tasks found.")
+    }
+
+    @Test
+    fun `getTaskById() should return success result with the task when the task is found`() {
+        // Given
+        val taskId = "1"
+        val taskList = listOf(
+            Task(id = "1", name = "Task 1", description = "Description 1", stateId = "state1", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0)),
+            Task(id = "2", name = "Task 2", description = "Description 2", stateId = "state2", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0))
+        )
+        every { taskRepository.getAllTasks() } returns Result.success(taskList)
+
+        // When
+        val result = manageTasksUseCase.getTaskById(taskId)
+
+        // Then
+        assertThat(result.getOrNull()).isEqualTo(taskList.find { it.id == taskId })
+    }
+
+    @Test
+    fun `getTaskById() should return failure result when the task is not found`() {
+        // Given
+        val taskId = "3"
+        val taskList = listOf(
+            Task(id = "1", name = "Task 1", description = "Description 1", stateId = "state1", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0)),
+            Task(id = "2", name = "Task 2", description = "Description 2", stateId = "state2", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0))
+        )
+        every { taskRepository.getAllTasks() } returns Result.success(taskList)
+
+        // When
+        val result = manageTasksUseCase.getTaskById(taskId)
+
+        // Then
+        assertThat(result.exceptionOrNull()?.message).isEqualTo("Task with ID '3' not found.")
+    }
+
+    @Test
+    fun `createTask() should return success result when the task is successfully created and linked to a project`() {
+        // Given
+        val task = Task(id = "1", name = "Task 1", description = "Description 1", stateId = "state1", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0))
+        val taskInProject = TaskInProject(taskId = "1", projectId = "project1", stateId = "state1")
+        every { taskRepository.createTask(task) } returns Result.success(true)
+        every { taskRepository.linkTaskToProject(taskInProject) } returns Result.success(true)
+
+        // When
+        val result = manageTasksUseCase.createTask(task, taskInProject)
+
+        // Then
+        assertThat(result.getOrNull()).isTrue()
+    }
+
+    @Test
+    fun `createTask() should return failure result when the task already exists`() {
+        // Given
+        val task = Task(id = "1", name = "Task 1", description = "Description 1", stateId = "state1", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0))
+        val taskInProject = TaskInProject(taskId = "1", projectId = "project1", stateId = "state1")
+        every { taskRepository.createTask(task) } returns Result.failure(Exception("Task already exists"))
+
+        // When
+        val result = manageTasksUseCase.createTask(task, taskInProject)
+
+        // Then
+        assertThat(result.exceptionOrNull()?.message).isEqualTo("Task already exists")
+    }
+
+    @Test
+    fun `editTask() should return success result when the task is successfully updated`() {
+        // Given
+        val updatedTask = Task(id = "1", name = "Updated Task", description = "Updated Description", stateId = "state1", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0))
+        every { taskRepository.editTask(updatedTask) } returns Result.success(true)
+
+        // When
+        val result = manageTasksUseCase.editTask(updatedTask)
+
+        // Then
+        assertThat(result.getOrNull()).isTrue()
+    }
+
+    @Test
+    fun `editTask() should return failure result when the task cannot be updated`() {
+        // Given
+        val updatedTask = Task(id = "1", name = "Updated Task", description = "Updated Description", stateId = "state1", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0))
+        every { taskRepository.editTask(updatedTask) } returns Result.failure(Exception("Failed to update task"))
+
+        // When
+        val result = manageTasksUseCase.editTask(updatedTask)
+
+        // Then
+        assertThat(result.exceptionOrNull()?.message).isEqualTo("Failed to update task")
+    }
+
+    @Test
+    fun `deleteTask() should return success result when the task is successfully deleted`() {
+        // Given
+        val taskId = "1"
+        val taskList = listOf(
+            Task(id = "1", name = "Task 1", description = "Description 1", stateId = "state1", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0))
+        )
+        every { taskRepository.getAllTasks() } returns Result.success(taskList)
+        every { taskRepository.deleteTask(taskId) } returns Result.success(true)
+
+        // When
+        val result = manageTasksUseCase.deleteTask(taskId)
+
+        // Then
+        assertThat(result.getOrNull()).isTrue()
+    }
+
+    @Test
+    fun `deleteTask() should return failure result when the task is not found`() {
+        // Given
+        val taskId = "3"
+        val taskList = listOf(
+            Task(id = "1", name = "Task 1", description = "Description 1", stateId = "state1", createdDate = LocalDateTime(2025, 4, 29, 12, 0, 0), updatedDate = LocalDateTime(2025, 4, 29, 12, 0, 0))
+        )
+        every { taskRepository.getAllTasks() } returns Result.success(taskList)
+
+        // When
+        val result = manageTasksUseCase.deleteTask(taskId)
+
+        // Then
+        assertThat(result.exceptionOrNull()?.message).isEqualTo("Task with ID '3' not found.")
+    }
+
+    @Test
+    fun `getAllMateTaskAssignment() should return success result with a list of mate task assignments`() {
+        // Given
+        val userName = "user1"
+        val assignments = listOf(
+            MateTaskAssignment(user = "user1", taskId = "1"),
+            MateTaskAssignment(user = "user1", taskId = "2")
+        )
+        every { taskRepository.getAllMateTaskAssignment(userName) } returns Result.success(assignments)
+
+        // When
+        val result = manageTasksUseCase.getAllMateTaskAssignment(userName)
+
+        // Then
+        assertThat(result.getOrNull()).isEqualTo(assignments)
+    }
+
+    @Test
+    fun `getAllMateTaskAssignment() should return failure result when no assignments are found`() {
+        // Given
+        val userName = "user1"
+        every { taskRepository.getAllMateTaskAssignment(userName) } returns Result.success(emptyList<MateTaskAssignment>())
+
+        // When
+        val result = manageTasksUseCase.getAllMateTaskAssignment(userName)
+
+        // Then
+        assertThat(result.exceptionOrNull()?.message).isEqualTo("No tasks found for user 'user1'.")
+    }
+
+    @Test
+    fun `getAllTasksByProjectId() should return success result with tasks filtered by project ID`() {
+        // Given
+        val projectId = "project1"
+        val links = listOf(
+            TaskInProject(taskId = "1", projectId = "project1", stateId = "state1"),
+            TaskInProject(taskId = "2", projectId = "project2", stateId = "state2")
+        )
+        every { taskRepository.getAllTasksInProjects() } returns Result.success(links)
+
+        // When
+        val result = manageTasksUseCase.getAllTasksByProjectId(projectId)
+
+        // Then
+        assertThat(result.getOrNull()).isEqualTo(links.filter { it.projectId == projectId })
+    }
+
+    @Test
+    fun `getAllTasksByProjectId() should return failure result when no tasks are found for the project`() {
+        // Given
+        val projectId = "project3"
+        val links = listOf(
+            TaskInProject(taskId = "1", projectId = "project1", stateId = "state1"),
+            TaskInProject(taskId = "2", projectId = "project2", stateId = "state2")
+        )
+        every { taskRepository.getAllTasksInProjects() } returns Result.success(links)
+
+        // When
+        val result = manageTasksUseCase.getAllTasksByProjectId(projectId)
+
+        // Then
+        assertThat(result.exceptionOrNull()?.message).isEqualTo("No tasks found for project 'project3'.")
+    }
+}
