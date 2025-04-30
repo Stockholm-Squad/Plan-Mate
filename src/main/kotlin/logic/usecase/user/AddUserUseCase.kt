2406a983470f96cb611f74ac79aefe49f1a5a56b
Hager-Gomaa <hagargomaa313@gmail.com>
2025-04-30 19:59:13 +0300

    refactor

--- src/main/kotlin/logic/usecase/user/AddUserUseCase.kt
+++ src/main/kotlin/logic/usecase/user/AddUserUseCase.kt
@@ -2,20 +2,32 @@ package org.example.logic.usecase.user
 
 import logic.model.entities.User
 import org.example.logic.repository.AuthenticationRepository
+import java.security.MessageDigest
 
-class AddUserUseCase(val authenticationRepository: AuthenticationRepository) {
+class AddUserUseCase(private val authenticationRepository: AuthenticationRepository) {
 
-        fun addUser(newUser: User): Result<Boolean> {
-            return authenticationRepository.getAllUsers()
-                .mapCatching { existingUsers ->
-                    // Validate no duplicate username
-                    if (existingUsers.any { it.username == newUser.username }) {
-                        throw IllegalArgumentException("User ${newUser.username} already exists")
-                    }
-
-                    // Append new user to file
-                    authenticationRepository.addUser(newUser).getOrThrow()
+    fun addUser(newUser: User): Result<Boolean> {
+        val newUserWithHashedPassword = User(newUser.username, md5Hash(newUser.hashedPassword))
+        return authenticationRepository.getAllUsers()
+            .mapCatching { existingUsers ->
+                // Validate no duplicate username
+                if (existingUsers.any { it.username == newUser.username }) {
+                    throw IllegalArgumentException("User ${newUser.username} already exists")
                 }
-        }
 
+                // Append new user to file
+                authenticationRepository.addUser(newUserWithHashedPassword).getOrThrow()
+            }
+    }
+
+    private fun md5Hash(password: String): String {
+        return try {
+            val md = MessageDigest.getInstance("MD5")
+            val digested = md.digest(password.toByteArray())
+            digested.fold("") { str, byte -> str + "%02x".format(byte) }
+        } catch (e: Exception) {
+            throw RuntimeException("Failed to hash password", e)
+        }
+    }
 }
+
