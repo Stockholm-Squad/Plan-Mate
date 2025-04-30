47fd7271d9d7980b49cfda9c07378eb2c2d1404b
Thoraya <thoriahamdy99@gmail.com>
2025-04-30 09:43:22 +0300

    add state data source implementation.

--- src/main/kotlin/data/datasources/StateCsvDataSource.kt
+++ src/main/kotlin/data/datasources/StateCsvDataSource.kt
@@ -1,14 +1,28 @@
 package org.example.data.datasources
 
 import logic.model.entities.State
+import org.example.logic.model.exceptions.PlanMateExceptions
+import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.api.cast
+import org.jetbrains.kotlinx.dataframe.api.toList
+import org.jetbrains.kotlinx.dataframe.io.readCSV
 
-class StateCsvDataSource : PlanMateDataSource<State> {
+class StateCsvDataSource(
+    private val filePath: String
+) : CsvDataSource<State>(filePath) {
     override fun read(): Result<List<State>> {
-        return Result.success(listOf())
+        super.resolveFile().also {
+            if (!it.exists()) return Result.failure(PlanMateExceptions.DataException.FileNotExistException())
+        }
+
+        return try {
+            DataFrame.readCSV(filePath).cast<State>().toList().let {
+                Result.success(it)
+            }
+        } catch (t: Throwable) {
+            Result.failure(PlanMateExceptions.DataException.ReadException())
+        }
     }
 
-    override fun write(model: List<State>): Result<Boolean> {
-        return Result.success(false)
-    }
 
 }
