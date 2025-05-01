29e9fd7c51d77ffac35cf44428e2ead4c725aa8c
Thoraya <thoriahamdy99@gmail.com>
2025-05-01 15:38:24 +0300

    resolve conflicts due to merge.

--- src/main/kotlin/input_output/output/OutputPrinterImplementation.kt
+++ src/main/kotlin/input_output/output/OutputPrinterImplementation.kt
@@ -1,7 +1,7 @@
 package org.example.input_output.output
 
-import logic.model.entities.State
 import logic.model.entities.AuditSystem
+import logic.model.entities.State
 
 class OutputPrinterImplementation : OutputPrinter {
     override fun showMessage(message: String) {
@@ -33,20 +33,28 @@ class OutputPrinterImplementation : OutputPrinter {
 
         for ((entityType, entries) in groupedByEntityType) {
             println("\n========== $entityType ==========\n")
-            println(String.format("| %-36s | %-12s | %-15s | %-20s | %-30s |",
-                "ID", "Entity ID", "Changed By", "Date", "Change Description"))
+            println(
+                String.format(
+                    "| %-36s | %-12s | %-15s | %-20s | %-30s |",
+                    "ID", "Entity ID", "Changed By", "Date", "Change Description"
+                )
+            )
             println("-".repeat(130))
 
             for (entry in entries) {
-                println(String.format("| %-36s | %-12s | %-15s | %-20s | %-30s |",
-                    entry.id,
-                    entry.entityId,
-                    entry.changedBy,
-                    entry.dateTime,
-                    entry.changeDescription.take(30)
-                ))
+                println(
+                    String.format(
+                        "| %-36s | %-12s | %-15s | %-20s | %-30s |",
+                        entry.id,
+                        entry.entityId,
+                        entry.changedBy,
+                        entry.dateTime,
+                        entry.changeDescription.take(30)
+                    )
+                )
             }
 
             println("-".repeat(130)) // Swimlane separator
         }
+    }
 }
