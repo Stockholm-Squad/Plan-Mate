f95a4e5fa582d88ca139aa8067536f6c63049114
Hager-Gomaa <hagargomaa313@gmail.com>
2025-05-01 16:41:16 +0300

    refactor

--- src/main/kotlin/logic/usecase/user/AddUserUseCase.kt
+++ src/main/kotlin/logic/usecase/user/AddUserUseCase.kt
@@ -1,42 +1,63 @@
 package org.example.logic.usecase.user
 
 import logic.model.entities.User
+import org.example.logic.model.exceptions.PlanMateExceptions
 import org.example.logic.repository.UserRepository
 import org.example.utils.hashToMd5
-import java.security.MessageDigest
+
 class AddUserUseCase(private val userRepository: UserRepository) {
 
-    fun addUser(newUser: User): Result<Boolean> {
-        return userRepository.getAllUsers()
-            .fold(
-                onSuccess = { onSuccessHandler(newUser, it) },
-                onFailure = { error -> Result.failure(error) }
-            )
+    fun addUser(username: String, password: String): Result<Boolean> {
+        return runCatching {
+            validateUserName(username)
+            validatePassword(password)
+
+            userRepository.getAllUsers().fold(
+                onSuccess = {
+                    handleSuccess(username = username, password = password, users = it).fold(
+                        onSuccess = { userRepository.addUser(it) },
+                        onFailure = { handleFailure(it as PlanMateExceptions) })
+                },
+                onFailure = { handleFailure(it as PlanMateExceptions.LogicException.UsersIsEmpty) })
+        }.fold(
+            onSuccess = { Result.success(true) },
+            onFailure = { Result.failure(exception = it) })
     }
 
-    private fun onSuccessHandler(
-        newUser: User,
-        existingUsers: List<User>
-    ) = try {
-        validateNewUser(newUser, existingUsers)
-        val hashedUser = createHashedUser(newUser)
-        userRepository.addUser(hashedUser)
-    } catch (e: Exception) {
-        Result.failure(e)
+
+    private fun validateUserName(username: String) {
+        if (username.isBlank() || username.length > 20 || username.length < 4 || username.first()
+                .isDigit()
+        ) throw PlanMateExceptions.LogicException.InvalidUserName()
     }
 
-    private fun validateNewUser(newUser: User, existingUsers: List<User>) {
-        if (existingUsers.any { it.username == newUser.username }) {
-            throw IllegalArgumentException("User ${newUser.username} already exists")
-        }
+    private fun validatePassword(password: String) {
+        if (password.isBlank() || password.length < 8) throw PlanMateExceptions.LogicException.InvalidPassword()
     }
 
-     fun createHashedUser(user: User): User {
-        return try {
-            User(user.username, hashToMd5(user.hashedPassword))
-        } catch (e: Exception) {
-            throw RuntimeException("Failed to hash password", e)
-        }
+    private fun handleSuccess(
+        username: String,
+        password: String,
+        users: List<User>
+    ): Result<User> {
+        return runCatching {
+            checkUserExists(users, username)
+        }.fold(
+            onSuccess = { Result.success(User(username, hashedPassword = hashToMd5(password))) },
+            onFailure = { Result.failure(it as PlanMateExceptions) })
+
+    }
+
+    private fun handleFailure(exceptions: PlanMateExceptions): Result<Boolean> {
+        return Result.failure(exceptions)
+    }
+
+    fun checkUserExists(users: List<User>, username: String) {
+        users.find { it.username == username }.let { throw PlanMateExceptions.LogicException.UserExist() }
+
     }
 
 }
+
+
+
