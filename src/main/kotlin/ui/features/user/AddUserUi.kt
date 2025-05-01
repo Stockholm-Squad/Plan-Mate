4e2bdfe64232aa58965ba96dc86ba5d7e2f4ab7c
Hager-Gomaa <hagargomaa313@gmail.com>
2025-05-01 16:57:16 +0300

    refactor add user ui

--- src/main/kotlin/ui/features/user/AddUserUi.kt
+++ src/main/kotlin/ui/features/user/AddUserUi.kt
@@ -1,7 +1,6 @@
 // AddUserUi.kt
 package org.example.ui.features.user
 
-import logic.model.entities.User
 import org.example.input_output.input.InputReader
 import org.example.input_output.output.OutputPrinter
 import org.example.logic.usecase.user.AddUserUseCase
@@ -16,25 +15,23 @@ class AddUserUi(
         // Get username input
         printer.showMessage("Enter username:")
         val username = inputReader.readStringOrNull()
-
         // Get password input
         printer.showMessage("Enter password:")
         val password = inputReader.readStringOrNull()
 
         // Create user if both inputs are valid
-        if (username != null && password != null) {
-            val user = User(username, password)
-            addUser(user)
+        if (username?.isNotEmpty() == true && password?.isNotEmpty() == true) {
+            addUser(username, password)
         } else {
             printer.showMessage("❌ Error: Username and password cannot be empty")
         }
     }
 
-    fun addUser(user: User) {
-        addUserUseCase.addUser(user)
+    private fun addUser(username: String, password: String) {
+        addUserUseCase.addUser(username, password)
             .onSuccess { success ->
                 if (success) {
-                    printer.showMessage("✅ User ${user.username} added successfully!")
+                    printer.showMessage("✅ User ${username} added successfully!")
                 } else {
                     printer.showMessage("❌ Error: Failed to add user")
                 }
