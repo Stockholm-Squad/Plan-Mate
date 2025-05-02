8b46a182288f69da97878261ddbe5d17253e20ac
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-05-02 23:32:28 +0300

    Update Extentions.kt

--- src/main/kotlin/data/extention/Extentions.kt
+++ src/main/kotlin/data/extention/Extentions.kt
@@ -3,13 +3,14 @@ package org.example.data.extention
 import kotlinx.datetime.LocalDateTime
 import java.util.*
 
-
+//TODO
 fun String.toSafeUUID(): UUID = try {
     UUID.fromString(this)
 } catch (ex: IllegalArgumentException) {
     throw Exception("Invalid UUID format: $this")
 }
 
+//TODO
 fun String.toLocalDateTime(): LocalDateTime = try {
     LocalDateTime.parse(this)
 } catch (ex: IllegalArgumentException) {
