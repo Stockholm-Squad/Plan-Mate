fd3c5aaf6dbbdf9b467136c7e801bd844b00382f
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-05-02 13:34:54 +0300

    Create Add Admin Temp File

---
+++ src/main/kotlin/AddAdminTempFile.kt
@@ -0,0 +1,19 @@
+package org.example
+
+import logic.model.entities.Role
+import logic.model.entities.User
+import org.example.data.datasources.user_data_source.UserCsvDataSource
+import org.example.utils.hashToMd5
+
+
+fun main() {
+    val dataSource = UserCsvDataSource("users.csv")
+    val adminUser = listOf(
+        User("rodina", hashToMd5("admin123"), Role.ADMIN),
+    )
+    val result = dataSource.overWrite(adminUser)
+    result.onSuccess { println("Appended tasks successfully") }
+        .onFailure { it.printStackTrace() }
+    dataSource.read().onSuccess { println("Current tasks:\n$it") }
+        .onFailure { it.printStackTrace() }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,12 @@
 package org.example
 
-import logic.model.entities.Role
-import logic.model.entities.User
-import org.example.data.datasources.user_data_source.UserCsvDataSource
 import org.example.di.appModule
 import org.example.ui.PlanMateConsoleUi
-import org.example.utils.hashToMd5
 import org.koin.core.context.startKoin
 import org.koin.core.logger.PrintLogger
 import org.koin.mp.KoinPlatform.getKoin
 
 fun main() {
-//    val dataSource = UserCsvDataSource("users.csv")
-//    val adminUser = listOf(
-//        User("rodina", hashToMd5("admin123"), Role.ADMIN),
-//    )
-//    val result = dataSource.overWrite(adminUser)
-//    result.onSuccess { println("Appended tasks successfully") }
-//        .onFailure { it.printStackTrace() }
-//    dataSource.read().onSuccess { println("Current tasks:\n$it") }
-//        .onFailure { it.printStackTrace() }
-//
-
     startKoin {
         logger(PrintLogger())
         modules(appModule)
@@ -29,4 +14,3 @@ fun main() {
     val planMateConsoleUi: PlanMateConsoleUi = getKoin().get()
     planMateConsoleUi.invoke()
 }
-
