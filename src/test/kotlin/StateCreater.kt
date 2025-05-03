1c75ff00ae455b236d0e3db637e02806377b88a8
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-05-03 21:04:09 +0300

    solve errors in repo tests

--- src/test/kotlin/StateCreater.kt
+++ src/test/kotlin/StateCreater.kt
@@ -1,8 +1,9 @@
 import logic.model.entities.ProjectState
+import org.example.logic.usecase.extention.toSafeUUID
 
 fun createState(
     id: String,
     name: String,
-):ProjectState{
-return ProjectState(id=id,name=name)
+): ProjectState {
+    return ProjectState(id = id.toSafeUUID(), name = name)
 }
