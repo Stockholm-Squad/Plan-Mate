f7b40151c7cad4d93ce7e9e6b6ffdab82d7bdd7b
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-05-03 17:31:15 +0300

    Add buildTaskModel

--- src/test/kotlin/utils/buildTest.kt
+++ src/test/kotlin/utils/buildTest.kt
@@ -2,14 +2,15 @@ package utils
 
 import kotlinx.datetime.LocalDateTime
 import logic.model.entities.Task
+import org.example.data.models.TaskModel
 import org.example.data.utils.DateHandlerImp
 import java.util.UUID
 
 fun buildTask(
-    id: String = UUID.randomUUID().toString(),
+    id: UUID= UUID.randomUUID(),
     name: String = "",
     description: String = "",
-    stateId: String = "",
+    stateId: UUID,
     createdDate: LocalDateTime = DateHandlerImp().getCurrentDateTime(),
     updatedDate: LocalDateTime = DateHandlerImp().getCurrentDateTime()
 
@@ -17,4 +18,16 @@ fun buildTask(
     return Task(
         id, name, description,stateId, createdDate, updatedDate
     )
+}
+
+
+fun buildTaskModel(
+    id: String = UUID.randomUUID().toString(),
+    name: String = "",
+    description: String = "",
+    stateId: String = "",
+    createdDate: String = DateHandlerImp().getCurrentDateTime().toString(),
+    updatedDate: String = DateHandlerImp().getCurrentDateTime().toString()
+): TaskModel {
+    return TaskModel(id, name, description, stateId, createdDate, updatedDate)
 }
