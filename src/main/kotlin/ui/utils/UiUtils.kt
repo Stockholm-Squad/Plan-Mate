173ab4150707133b20f0d37cddb8651640175e7c
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-05-04 23:26:04 +0300

    Remove duplicate functions

--- src/main/kotlin/ui/utils/UiUtils.kt
+++ src/main/kotlin/ui/utils/UiUtils.kt
@@ -9,7 +9,7 @@ class UiUtils(
 ) {
 
     fun readNonBlankInputOrNull(reader: InputReader): String? {
-        return reader.readStringOrNull()?.takeIf { it.isNotBlank() }
+        return reader.readStringOrNull()?.takeIf { it.isNotBlank() }?.lowercase()
     }
 
     fun getEnteredOption(option: Int?) = TaskOptions.entries.find { it.option == option }
