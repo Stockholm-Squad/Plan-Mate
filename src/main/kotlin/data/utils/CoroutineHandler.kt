d10165213b77d56b1e58f731c7dc55606888880d
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-05-05 22:45:23 +0300

    Add executeSafelyWithContext fun

---
+++ src/main/kotlin/data/utils/CoroutineHandler.kt
@@ -0,0 +1,19 @@
+package org.example.data.utils
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+
+suspend fun <T> executeSafelyWithContext(
+    dispatcher: CoroutineDispatcher = Dispatchers.IO,
+    onSuccess: suspend () -> T,
+    onFailure: (ex: Exception) -> T
+): T = withContext(dispatcher) {
+    try {
+        onSuccess()
+    } catch (ex: Exception) {
+        onFailure(ex)
+    }
+}
+
