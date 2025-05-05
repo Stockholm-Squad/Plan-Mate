6b37eec518ca03c1a2ccc84af7240a89b5533f13
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-05-06 02:54:57 +0300

    add mongo Date Source and remove Result from StateDataSource

--- src/main/kotlin/data/datasources/task_In_project_data_source/TaskInProjectMongoDataSource.kt
+++ src/main/kotlin/data/datasources/task_In_project_data_source/TaskInProjectMongoDataSource.kt
@@ -1,15 +1,10 @@
 package org.example.data.datasources.task_In_project_data_source
 
 import data.models.TaskInProjectModel
-import data.models.UserAssignedToProjectModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.example.data.datasources.MongoSetup
-import org.example.data.datasources.task_data_source.ITaskDataSource
-import org.example.data.models.UserModel
 import org.example.data.utils.TASK_IN_PROJECT_COLLECTION_NAME
-import org.example.data.utils.USERS_COLLECTION_NAME
-import org.example.data.utils.USER_ASSIGNED_TO_PROJECT_COLLECTION_NAME
 
 class TaskInProjectMongoDataSource() : ITaskInProjectDataSource {
 
