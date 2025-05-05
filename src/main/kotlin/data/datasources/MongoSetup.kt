cc3c80af37414ca993ae66393724b02eb55750b5
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-05-06 01:42:30 +0300

    Update MongoSetup.kt

--- src/main/kotlin/data/datasources/MongoSetup.kt
+++ src/main/kotlin/data/datasources/MongoSetup.kt
@@ -1,11 +1,16 @@
 package org.example.data.datasources
 
+import org.litote.kmongo.coroutine.CoroutineClient
+import org.litote.kmongo.coroutine.CoroutineDatabase
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
 object MongoSetup {
+
     private const val CONNECTION_STRING =
-        "mongodb+srv://asmaa:S6H6nUZ1JMYyQ1Xq@cluster0.6yjjmpq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    val client = KMongo.createClient(CONNECTION_STRING).coroutine
-    val database = client.getDatabase("PlanMate")
+        "mongodb+srv://PlanMate:planmate@planmate.06s0n1e.mongodb.net/?retryWrites=true&w=majority&appName=PlanMate"
+
+    val client: CoroutineClient = KMongo.createClient(CONNECTION_STRING).coroutine
+
+    val database: CoroutineDatabase = client.getDatabase("PlanMate")
 }
