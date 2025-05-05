cbb42d0ab2287e50ede0697d832423708199353d
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-05-06 02:32:57 +0300

    Revert "add mongo Date Source and remove Result from UserAssignedToProjectCsvDataSource"
    This reverts commit 3ca0a0bd4f57b75e2b8f7dc18c0767ddceba924d.

--- src/main/kotlin/data/datasources/user_assigned_to_project_data_source/UserAssignedToProjectCsvDataSource.kt
+++ src/main/kotlin/data/datasources/user_assigned_to_project_data_source/UserAssignedToProjectCsvDataSource.kt
@@ -1,7 +1,6 @@
 package org.example.data.datasources.user_assigned_to_project_data_source
 
 import data.models.UserAssignedToProjectModel
-import logic.models.exceptions.DataException
 import logic.models.exceptions.FileNotExistException
 import logic.models.exceptions.ReadDataException
 import logic.models.exceptions.WriteDataException
@@ -18,36 +17,39 @@ import java.io.File
 class UserAssignedToProjectCsvDataSource(private val filePath: String) : IUserAssignedToProjectDataSource {
     private fun resolveFile(): File = File(filePath)
 
-    override fun read(): List<UserAssignedToProjectModel> {
+    override fun read(): Result<List<UserAssignedToProjectModel>> {
         val file = resolveFile()
         if (!file.exists()) {
-            file.createNewFile()
+            try {
+                file.createNewFile()
+            } catch (throwable: Throwable) {
+                return Result.failure(FileNotExistException())
+            }
         }
 
-        try {
+        return try {
             if (File(filePath).readLines().size < 2)
                 return Result.success(emptyList())
 
             val users = DataFrame.readCSV(file)
                 .cast<UserAssignedToProjectModel>()
                 .toList()
-        return
             Result.success(users)
         } catch (e: Exception) {
-            Result.failure(DataException.ReadDataException())
+            Result.failure(ReadDataException())
         }
     }
 
-    override fun overWrite(users: List<UserAssignedToProjectModel>): Boolean {
+    override fun overWrite(users: List<UserAssignedToProjectModel>): Result<Boolean> {
         return try {
             users.toDataFrame().writeCSV(resolveFile())
             Result.success(true)
         } catch (e: Exception) {
-            Result.failure(DataException.WriteDataException())
+            Result.failure(WriteDataException())
         }
     }
 
-    override fun append(users: List<UserAssignedToProjectModel>): Boolean {
+    override fun append(users: List<UserAssignedToProjectModel>): Result<Boolean> {
         return try {
             resolveFile().also { file ->
                 val existing = if (file.exists() && file.length() > 0) {
@@ -59,7 +61,7 @@ class UserAssignedToProjectCsvDataSource(private val filePath: String) : IUserAs
             }
             Result.success(true)
         } catch (e: Exception) {
-            Result.failure(DataException.WriteDataException())
+            Result.failure(WriteDataException())
         }
     }
 }
