c26147360ce86cb47696b2df211bd8ceacc0b0ca
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-05-05 07:15:48 +0300

    add interface to task ui

--- src/main/kotlin/logic/repository/UserRepository.kt
+++ src/main/kotlin/logic/repository/UserRepository.kt
@@ -6,11 +6,9 @@ import java.util.*
 interface UserRepository {
     fun addUser(user: User): Result<Boolean>
     fun getAllUsers(): Result<List<User>>
-
     fun getUsersByProjectId(projectId: UUID): Result<List<User>>
     fun addUserToProject(projectId: UUID, userName: String): Result<Boolean>
     fun deleteUserFromProject(projectId: UUID, userName: String): Result<Boolean>
-
     fun addUserToTask(mateName: String, taskId: UUID): Result<Boolean>
     fun deleteUserFromTask(mateName: String, taskId: UUID): Result<Boolean>
 }
