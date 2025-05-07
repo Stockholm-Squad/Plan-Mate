797ded6f0f8414b264ad2fd5e2651cc9a47c6830
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-05-07 23:06:26 +0300

    Update StateMongoDataSource & StateCsvDataSource & IStateDataSource Update append & overwrite

--- src/main/kotlin/data/datasources/state_data_source/IStateDataSource.kt
+++ src/main/kotlin/data/datasources/state_data_source/IStateDataSource.kt
@@ -4,6 +4,6 @@ import org.example.data.models.ProjectStateModel
 
 interface IStateDataSource {
     suspend fun read(): List<ProjectStateModel>
-    suspend fun overWrite(users: List<ProjectStateModel>): Boolean
-    suspend fun append(users: List<ProjectStateModel>): Boolean
+    suspend fun overWrite(state: List<ProjectStateModel>): Boolean
+    suspend fun append(state: List<ProjectStateModel>): Boolean
 }
--- src/main/kotlin/data/datasources/state_data_source/StateMongoDataSource.kt
+++ src/main/kotlin/data/datasources/state_data_source/StateMongoDataSource.kt
@@ -1,9 +1,11 @@
 package org.example.data.datasources.state_data_source
 
+import com.mongodb.client.result.DeleteResult
+import com.mongodb.client.result.InsertManyResult
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.example.data.models.ProjectStateModel
 import org.example.data.database.STATES_COLLECTION_NAME
+import org.example.data.models.ProjectStateModel
 import org.litote.kmongo.coroutine.CoroutineDatabase
 
 
@@ -15,14 +17,25 @@ class StateMongoDataSource(mongoDatabase: CoroutineDatabase) : IStateDataSource 
         collection.find().toList()
     }
 
-    override suspend fun overWrite(users: List<ProjectStateModel>): Boolean = withContext(Dispatchers.IO) {
-        collection.deleteMany()
-        collection.insertMany(users)
-        true
+    override suspend fun overWrite(state: List<ProjectStateModel>): Boolean = withContext(Dispatchers.IO) {
+        val deleteResult: DeleteResult = collection.deleteMany()
+        if (!deleteResult.wasAcknowledged()) {
+            return@withContext false
+        }
+
+        if (state.isEmpty()) {
+            return@withContext true
+        }
+
+        val insertResult: InsertManyResult = collection.insertMany(state)
+        return@withContext insertResult.wasAcknowledged() && insertResult.insertedIds.size == state.size
     }
 
-    override suspend fun append(users: List<ProjectStateModel>): Boolean = withContext(Dispatchers.IO) {
-        collection.insertMany(users)
-        true
+    override suspend fun append(state: List<ProjectStateModel>): Boolean = withContext(Dispatchers.IO) {
+        if (state.isEmpty()) {
+            return@withContext true
+        }
+        val insertResult: InsertManyResult = collection.insertMany(state)
+        return@withContext insertResult.wasAcknowledged() && insertResult.insertedIds.size == state.size
     }
 }
