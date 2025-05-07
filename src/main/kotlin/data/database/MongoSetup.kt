9a508c4cc6cd1a7835c8e7e4f07d51cef4291d3b
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-05-07 22:10:33 +0300

    add MongoSetup and it's Constants to database package

--- src/main/kotlin/data/database/MongoSetup.kt
+++ src/main/kotlin/data/database/MongoSetup.kt
@@ -1,15 +1,11 @@
-package org.example.data.datasources
+package org.example.data.database
 
-import org.example.data.utils.DATABASE_NAME
 import org.litote.kmongo.coroutine.CoroutineClient
 import org.litote.kmongo.coroutine.CoroutineDatabase
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
 object MongoSetup {
-
-    private const val CONNECTION_STRING =
-        "mongodb+srv://PlanMate:planmate@planmate.06s0n1e.mongodb.net/?retryWrites=true&w=majority&appName=PlanMate"
 
     val client: CoroutineClient = KMongo.createClient(CONNECTION_STRING).coroutine
     val database: CoroutineDatabase = client.getDatabase(DATABASE_NAME)
