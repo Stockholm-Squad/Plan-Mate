c59592f752b5f48cce595a59f2231a84adf46cb8
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-05-07 21:26:01 +0300

    SetUp Database & add Constants.kt

--- src/main/kotlin/data/datasources/MongoSetup.kt
+++ src/main/kotlin/data/datasources/MongoSetup.kt
@@ -1,5 +1,6 @@
 package org.example.data.datasources
 
+import org.example.data.utils.DATABASE_NAME
 import org.litote.kmongo.coroutine.CoroutineClient
 import org.litote.kmongo.coroutine.CoroutineDatabase
 import org.litote.kmongo.coroutine.coroutine
@@ -11,6 +12,5 @@ object MongoSetup {
         "mongodb+srv://PlanMate:planmate@planmate.06s0n1e.mongodb.net/?retryWrites=true&w=majority&appName=PlanMate"
 
     val client: CoroutineClient = KMongo.createClient(CONNECTION_STRING).coroutine
-
-    val database: CoroutineDatabase = client.getDatabase("PlanMate")
+    val database: CoroutineDatabase = client.getDatabase(DATABASE_NAME)
 }
