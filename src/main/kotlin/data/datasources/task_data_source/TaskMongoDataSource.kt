2d3752dba7eb9179a4234cbe5100cc9f219e857f
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-05-07 23:09:31 +0300

    Update TaskMongoDataSource Update append & overwrite

--- src/main/kotlin/data/datasources/task_data_source/TaskMongoDataSource.kt
+++ src/main/kotlin/data/datasources/task_data_source/TaskMongoDataSource.kt
@@ -1,9 +1,11 @@
 package org.example.data.datasources.task_data_source
 
+import com.mongodb.client.result.DeleteResult
+import com.mongodb.client.result.InsertManyResult
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.example.data.models.TaskModel
 import org.example.data.database.TASKS_COLLECTION_NAME
+import org.example.data.models.TaskModel
 import org.litote.kmongo.coroutine.CoroutineDatabase
 
 
@@ -15,14 +17,25 @@ class TaskMongoDataSource(mongoDatabase: CoroutineDatabase) : ITaskDataSource {
         collection.find().toList()
     }
 
-    override suspend fun overWrite(users: List<TaskModel>): Boolean = withContext(Dispatchers.IO) {
-        collection.deleteMany()
-        collection.insertMany(users)
-        true
+    override suspend fun overWrite(tasks: List<TaskModel>): Boolean = withContext(Dispatchers.IO) {
+        val deleteResult: DeleteResult = collection.deleteMany()
+        if (!deleteResult.wasAcknowledged()) {
+            return@withContext false
+        }
+
+        if (tasks.isEmpty()) {
+            return@withContext true
+        }
+
+        val insertResult: InsertManyResult = collection.insertMany(tasks)
+        return@withContext insertResult.wasAcknowledged() && insertResult.insertedIds.size == tasks.size
     }
 
-    override suspend fun append(users: List<TaskModel>): Boolean = withContext(Dispatchers.IO) {
-        collection.insertMany(users)
-        true
+    override suspend fun append(tasks: List<TaskModel>): Boolean = withContext(Dispatchers.IO) {
+        if (tasks.isEmpty()) {
+            return@withContext true
+        }
+        val insertResult: InsertManyResult = collection.insertMany(tasks)
+        return@withContext insertResult.wasAcknowledged() && insertResult.insertedIds.size == tasks.size
     }
 }
