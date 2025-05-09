becdff38bbe48d3c460cfaee9a5db5b187458bc8
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-05-09 17:44:02 +0300

    create UserDataSource and update UserRepositoryImp

--- src/main/kotlin/data/repo/UserRepositoryImp.kt
+++ src/main/kotlin/data/repo/UserRepositoryImp.kt
@@ -1,120 +1,101 @@
 package org.example.data.repo
 
+import data.datasources.user_data_source.UserDataSource
 import data.models.MateTaskAssignmentModel
 import data.models.UserAssignedToProjectModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
 import logic.models.entities.User
+import logic.models.exceptions.StateExceptions
 import logic.models.exceptions.UserExceptions
 import logic.models.exceptions.UserToProjectExceptions
 import logic.models.exceptions.UserToTaskExceptions
 import org.example.data.datasources.mate_task_assignment_data_source.IMateTaskAssignmentDataSource
+import org.example.data.datasources.mate_task_assignment_data_source.MateTaskAssignmentDataSource
 import org.example.data.datasources.user_assigned_to_project_data_source.IUserAssignedToProjectDataSource
+import org.example.data.datasources.user_assigned_to_project_data_source.UserAssignedToProjectDataSource
 import org.example.data.datasources.user_data_source.IUserDataSource
+import org.example.data.mapper.mapToStateModel
 import org.example.data.mapper.mapToUserEntity
 import org.example.data.mapper.mapToUserModel
 import org.example.data.utils.executeSafelyWithContext
+import org.example.data.utils.tryToExecute
 import org.example.logic.repository.UserRepository
 import org.example.logic.usecase.extention.toSafeUUID
 import java.util.*
 
 class UserRepositoryImp(
-    private val userDataSource: IUserDataSource,
-    private val userAssignedToProjectDataSource: IUserAssignedToProjectDataSource,
-    private val mateTaskAssignment: IMateTaskAssignmentDataSource,
+    private val userDataSource: UserDataSource,
+    private val userAssignedToProjectDataSource: UserAssignedToProjectDataSource,
+    private val mateTaskAssignment: MateTaskAssignmentDataSource,
 ) : UserRepository {
+
     override suspend fun addUser(user: User): Boolean =
-        executeSafelyWithContext(
-            onSuccess = {
-                userDataSource.append(listOf(user.mapToUserModel()))
-            },
-            onFailure = {
-                throw UserExceptions.UserNotAddedException()
-            }
+        tryToExecute({ userDataSource.addUser(user.mapToUserModel()) },
+            onSuccess = { it },
+            onFailure = { throw UserExceptions.UserNotAddedException() }
         )
 
     override suspend fun getAllUsers(): List<User> =
-        executeSafelyWithContext(
-            onSuccess = {
-                userDataSource.read()
-                    .mapNotNull {
-                        it.mapToUserEntity()
-                    }
-            },
-            onFailure = {
-                throw UserExceptions.UsersDoesNotExistException()
-            }
+        tryToExecute({ userDataSource.getAllUsers() },
+            onSuccess = { it.mapNotNull { userModel -> userModel.mapToUserEntity() } },
+            onFailure = { throw UserExceptions.UsersDoesNotExistException() }
         )
 
 
     override suspend fun getUsersByProjectId(projectId: UUID): List<User> =
-        executeSafelyWithContext(
-            onSuccess = {
-                val assignedUser = userAssignedToProjectDataSource.read().filter {
-                    it.projectId == projectId.toString()
-                }.map { it.userName }
-                val allUsers = userDataSource.read()
-                allUsers.filter { it.username in assignedUser }
-                    .mapNotNull { it.mapToUserEntity() }
-            },
-            onFailure = {
-                throw UserExceptions.UserDoesNotExistException()
+        withContext(Dispatchers.IO) {
+            val assignedUsersDeferred = async {
+                tryToExecute(
+                    function = { userDataSource.getUsersByProjectId(projectId.toString()) },
+                    onSuccess = { it },
+                    onFailure = { throw UserExceptions.UserDoesNotExistException() }
+                )
             }
-        )
+
+            val allUsersDeferred = async {
+                tryToExecute(
+                    function = { userDataSource.getAllUsers() },
+                    onSuccess = { it },
+                    onFailure = { throw UserExceptions.UserDoesNotExistException() }
+                )
+            }
+
+            val assignedUsers = assignedUsersDeferred.await()
+            val allUsers = allUsersDeferred.await()
+
+            val usersIds = assignedUsers.map { it.username }
+            allUsers.filter { it.username in usersIds }
+                .mapNotNull { it.mapToUserEntity() }
+        }
 
 
     override suspend fun addUserToProject(projectId: UUID, userName: String): Boolean =
-        executeSafelyWithContext(
-            onSuccess = {
-                userAssignedToProjectDataSource.append(
-                    listOf(UserAssignedToProjectModel(projectId = projectId.toString(), userName = userName))
-                )
-            }, onFailure = {
-                throw UserToProjectExceptions.UserNotAddedToProjectException()
-            }
+        tryToExecute({ userAssignedToProjectDataSource.addUserToProject(projectId.toString(), userName) },
+            onSuccess = { it },
+            onFailure = { throw UserToProjectExceptions.UserNotAddedToProjectException() }
         )
 
 
     override suspend fun deleteUserFromProject(projectId: UUID, userName: String): Boolean =
-        executeSafelyWithContext(
-            onSuccess = {
-                userAssignedToProjectDataSource.read().filter {
-                    (it.projectId.toSafeUUID() == projectId) && (it.userName == userName)
-                }.let { userAssignedToProjectDataSource.overWrite(it) }
-            },
-            onFailure = {
-                throw UserToProjectExceptions.UserNotDeletedFromProjectException()
-            }
+        tryToExecute({ userAssignedToProjectDataSource.deleteUserFromProject(projectId.toString(), userName) },
+            onSuccess = { it },
+            onFailure = { throw UserToProjectExceptions.UserNotDeletedFromProjectException() }
         )
 
 
     override suspend fun addUserToTask(mateName: String, taskId: UUID): Boolean =
-        executeSafelyWithContext(
-            onSuccess = {
-                mateTaskAssignment.append(
-                    listOf(
-                        MateTaskAssignmentModel(userName = mateName, taskId = taskId.toString())
-                    )
-                )
-            }, onFailure = {
+        tryToExecute({ mateTaskAssignment.addUserToTask(mateName, taskId.toString()) },
+            onSuccess = { it },
+            onFailure = {
                 throw UserToTaskExceptions.UserNotAddedToTaskException()
             }
         )
 
     override suspend fun deleteUserFromTask(mateName: String, taskId: UUID): Boolean =
-        executeSafelyWithContext(
-            onSuccess = {
-                val originalAssignments = mateTaskAssignment.read()
-                val newAssignments = mateTaskAssignment.read().filterNot {
-                    it.userName == mateName && it.taskId == taskId.toString()
-                }
-                return@executeSafelyWithContext if (newAssignments.size != originalAssignments.size) {
-                    mateTaskAssignment.overWrite(newAssignments)
-                    true
-                } else {
-                    false
-                }
-            },
-            onFailure = {
-                throw UserToTaskExceptions.UserNotDeletedFromTaskException()
-            }
+        tryToExecute({ mateTaskAssignment.deleteUserFromTask(mateName, taskId.toString()) },
+            onSuccess = { it },
+            onFailure = { throw UserToTaskExceptions.UserNotDeletedFromTaskException() }
         )
 }
