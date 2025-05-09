9d99a6e7d3735be05b12145bf02b0398128a61c3
Mona <monaayman584@gmail.com>
2025-05-09 19:11:07 +0300

    add getTaskInProjectByProjectId

--- src/main/kotlin/data/datasources/task_In_project_data_source/TaskInProjectDataSource.kt
+++ src/main/kotlin/data/datasources/task_In_project_data_source/TaskInProjectDataSource.kt
@@ -1,10 +1,12 @@
 package org.example.data.datasources.task_In_project_data_source
 
 import data.models.TaskInProjectModel
+import org.example.data.models.ProjectStateModel
 
 
 interface TaskInProjectDataSource {
     fun addTaskInProject(projectId: String, taskId: String): Boolean
     fun deleteTaskFromProject(projectId: String, taskId: String): Boolean
     fun getAllTasksInProject(): List<TaskInProjectModel>
+    fun getTaskInProjectByProjectId(projectId: String): ProjectStateModel
 }
