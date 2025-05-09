5c0f42633c6d3d3450be764ed879ddc5d2ece3c4
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-05-09 17:42:44 +0300

    create UserAssignedToProjectDataSource

--- src/main/kotlin/data/datasources/user_assigned_to_project_data_source/UserAssignedToProjectMongoDataSource.kt
+++ src/main/kotlin/data/datasources/user_assigned_to_project_data_source/UserAssignedToProjectMongoDataSource.kt
@@ -6,36 +6,42 @@ import data.models.UserAssignedToProjectModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.example.data.database.USER_ASSIGNED_TO_PROJECT_COLLECTION_NAME
+import org.litote.kmongo.and
 import org.litote.kmongo.coroutine.CoroutineDatabase
+import org.litote.kmongo.eq
+import java.util.*
 
-class UserAssignedToProjectMongoDataSource(mongoDatabase: CoroutineDatabase) : IUserAssignedToProjectDataSource {
+class UserAssignedToProjectMongoDataSource(mongoDatabase: CoroutineDatabase) : UserAssignedToProjectDataSource {
 
     private val collection = mongoDatabase.getCollection<UserAssignedToProjectModel>(USER_ASSIGNED_TO_PROJECT_COLLECTION_NAME)
 
+    override suspend fun addUserToProject(projectId: String, userName: String): Boolean {
+        val document = UserAssignedToProjectModel(
+            userName = userName,
+            projectId = projectId
+        )
 
-    override suspend fun read(): List<UserAssignedToProjectModel> = withContext(Dispatchers.IO) {
-        collection.find().toList()
+        val result = collection.insertOne(document)
+        return result.wasAcknowledged()
     }
 
-    override suspend fun overWrite(users: List<UserAssignedToProjectModel>): Boolean = withContext(Dispatchers.IO) {
-        val deleteResult: DeleteResult = collection.deleteMany()
-        if (!deleteResult.wasAcknowledged()) {
-            return@withContext false
-        }
+    override suspend fun deleteUserFromProject(projectId: String, userName: String): Boolean {
+        val filter = and(
+            UserAssignedToProjectModel::userName eq userName,
+            UserAssignedToProjectModel::projectId eq projectId
+        )
 
-        if (users.isEmpty()) {
-            return@withContext true
-        }
-
-        val insertResult: InsertManyResult = collection.insertMany(users)
-        return@withContext insertResult.wasAcknowledged() && insertResult.insertedIds.size == users.size
+        val result = collection.deleteOne(filter)
+        return result.deletedCount > 0
     }
 
-    override suspend fun append(users: List<UserAssignedToProjectModel>): Boolean = withContext(Dispatchers.IO) {
-        if (users.isEmpty()) {
-            return@withContext true
-        }
-        val insertResult: InsertManyResult = collection.insertMany(users)
-        return@withContext insertResult.wasAcknowledged() && insertResult.insertedIds.size == users.size
+    override suspend fun getUsersAssignedToProjectByProjectId(projectId: String): List<UserAssignedToProjectModel> {
+        val filter = UserAssignedToProjectModel::projectId eq projectId
+        return collection.find(filter).toList()
+    }
+
+    override suspend fun getUsersAssignedToProjectByUserName(userName: String): List<UserAssignedToProjectModel> {
+        val filter = UserAssignedToProjectModel::userName eq userName
+        return collection.find(filter).toList()
     }
 }
