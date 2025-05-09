f793c9efe9ca33401c077fefb784e848d00824d0
Thoraya <thoriahamdy99@gmail.com>
2025-05-09 06:37:19 +0300

    handle the usage of state data source implementation after changes the data sources

--- src/main/kotlin/logic/usecase/state/ManageStatesUseCase.kt
+++ src/main/kotlin/logic/usecase/state/ManageStatesUseCase.kt
@@ -1,7 +1,7 @@
 package org.example.logic.usecase.state
 
 import logic.models.entities.ProjectState
-import logic.models.exceptions.*
+import logic.models.exceptions.StateExceptions
 import org.example.logic.repository.ProjectStateRepository
 import org.example.logic.usecase.extention.isLetterOrWhiteSpace
 import org.example.logic.usecase.extention.isValidLength
@@ -13,18 +13,18 @@ class ManageStatesUseCase(
     suspend fun addProjectState(stateName: String): Boolean {
         return isStateNameValid(stateName).let { validStateName ->
             validStateName.takeIf { state ->
-                !isProjectStateExist(state)
+                !projectStateRepository.isProjectStateExist(state)
             }
         }
             ?.let {
-                projectStateRepository.addProjectState(it)
+                projectStateRepository.addProjectState(ProjectState(name = it))
             } ?: throw StateExceptions.StateAlreadyExistException()
     }
 
 
     suspend fun editProjectStateByName(stateName: String, newStateName: String): Boolean {
         return validateStateNames(stateName, newStateName).let { (validatedStateName, validatedNewStateName) ->
-            getProjectState(validatedStateName).let { state ->
+            projectStateRepository.getProjectStateByName(validatedStateName).let { state ->
                 projectStateRepository.editProjectState(ProjectState(state.id, validatedNewStateName))
             }
         }
@@ -32,7 +32,7 @@ class ManageStatesUseCase(
 
     suspend fun deleteProjectState(stateName: String): Boolean {
         return isStateNameValid(stateName).let { validStateName ->
-            getProjectState(validStateName).let { state ->
+            projectStateRepository.getProjectStateByName(validStateName).let { state ->
                 projectStateRepository.deleteProjectState(state)
             }
         }
@@ -44,13 +44,12 @@ class ManageStatesUseCase(
 
     suspend fun getProjectStateIdByName(stateName: String): UUID {
         return isStateNameValid(stateName).let {
-            getProjectState(stateName).id
+            projectStateRepository.getProjectStateByName(stateName).id
         }
     }
 
-    suspend fun getProjectStateNameByStateId(stateId: UUID): String? {
-        return getAllProjectStates()
-            .let { states -> findStateById(states, stateId)?.name }
+    suspend fun getProjectStateNameByStateId(stateId: UUID): String {
+        return projectStateRepository.getProjectStateByID(stateId).name
     }
 
     private fun validateStateNames(stateName: String, newStateName: String): Pair<String, String> {
@@ -67,25 +66,5 @@ class ManageStatesUseCase(
                     it.isValidLength(30) &&
                     it.isLetterOrWhiteSpace()
         } ?: throw StateExceptions.NotAllowedStateNameException()
-    }
-
-    private suspend fun isProjectStateExist(stateName: String): Boolean {
-        return try {
-                getProjectState(stateName)
-            true
-        }catch (e: Exception){
-            false
-        }
-    }
-
-    private suspend fun getProjectState(stateName: String): ProjectState {
-        return this.getAllProjectStates().let { allStates ->
-            allStates.firstOrNull { state -> state.name.equals(stateName, ignoreCase = true) }
-                ?: throw StateExceptions.StateNotExistException()
-        }
-    }
-
-    private fun findStateById(states: List<ProjectState>, stateId: UUID): ProjectState? {
-        return states.firstOrNull { it.id == stateId }
     }
 }
