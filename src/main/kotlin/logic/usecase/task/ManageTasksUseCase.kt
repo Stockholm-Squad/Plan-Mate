5574fe2a642fe78cff4d500391700e14863fff2c
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-05-10 13:24:36 +0300

    Remove fun audit log from AuditSystemRepository

--- src/main/kotlin/logic/usecase/task/ManageTasksUseCase.kt
+++ src/main/kotlin/logic/usecase/task/ManageTasksUseCase.kt
@@ -1,17 +1,13 @@
 package org.example.logic.usecase.task
 
-import org.example.logic.entities.AuditSystem
-import org.example.logic.entities.EntityType
-import org.example.logic.entities.Task
+
 import org.example.logic.TaskExceptions
-import org.example.data.utils.DateHandlerImp
-import org.example.logic.repository.AuditSystemRepository
+import org.example.logic.entities.Task
 import org.example.logic.repository.TaskRepository
 import java.util.*
 
 class ManageTasksUseCase(
     private val taskRepository: TaskRepository,
-    private val auditSystemRepository: AuditSystemRepository,
 ) {
     suspend fun getAllTasks(): List<Task> {
         return taskRepository.getAllTasks().takeIf { it.isNotEmpty() }
@@ -25,37 +21,25 @@ class ManageTasksUseCase(
     suspend fun getTaskIdByName(taskName: String): UUID =
         getTaskByName(taskName).id
 
-    suspend fun addTask(task: Task, userId: UUID, projectId: UUID): Boolean {
-        val isDuplicate = taskRepository.getTasksInProject(projectId = projectId)
-            .filter { it.name.equals(task.name, ignoreCase = true) } !== emptyList<Task>()
-
+    suspend fun addTask(task: Task, projectId: UUID): Boolean {
+        val isDuplicate =
+            taskRepository.getTasksInProject(projectId = projectId).any {
+                it.name.equals(task.name, ignoreCase = true)
+            }
         if (isDuplicate) throw TaskExceptions.DuplicateTaskNameException()
 
         return taskRepository.addTask(task).also { isAdded ->
-            if (isAdded) logAudit(task, userId)
-            else throw TaskExceptions.TaskNotAddedException()
+            if (!isAdded) throw TaskExceptions.TaskNotAddedException()
         }
     }
 
-    suspend fun editTask(updatedTask: Task, userId: UUID): Boolean =
+    suspend fun editTask(updatedTask: Task): Boolean =
         taskRepository.editTask(updatedTask).also { isUpdated ->
-            if (isUpdated) logAudit(updatedTask, userId)
-            else throw TaskExceptions.TaskNotEditException()
+            if (isUpdated) throw TaskExceptions.TaskNotEditException()
         }
 
     suspend fun deleteTaskByName(taskName: String): Boolean =
         taskRepository.deleteTask(getTaskIdByName(taskName)).also { isDeleted ->
             if (!isDeleted) throw TaskExceptions.TaskNotDeletedException()
         }
-
-    private suspend fun logAudit(updatedTask: Task, userId: UUID) {
-        val auditEntry = AuditSystem(
-            entityType = EntityType.TASK,
-            description = "update task ${updatedTask.name}",
-            userId = userId,
-            dateTime = DateHandlerImp().getCurrentDateTime(),
-            entityTypeId = updatedTask.id
-        )
-        auditSystemRepository.addAuditsEntries(listOf(auditEntry))
-    }
 }
