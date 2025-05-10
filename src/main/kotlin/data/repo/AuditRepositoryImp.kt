80166e92fa0cc64e1705485df1784c57e6b50264
Ahmed <ahmedsaaalah21@gmail.com>
2025-05-10 14:23:25 +0300

    refactored the AuditRepositoryImp.kt to user tryToExecute

--- src/main/kotlin/data/repo/AuditRepositoryImp.kt
+++ src/main/kotlin/data/repo/AuditRepositoryImp.kt
@@ -2,33 +2,29 @@ package org.example.data.repo
 
 import data.mapper.mapToAuditEntity
 import data.mapper.mapToAuditModel
-import org.example.data.datasources.IAuditDataSource
 import org.example.data.source.AuditDataSource
-import org.example.data.utils.executeSafelyWithContext
+import org.example.data.utils.tryToExecute
 import org.example.logic.AuditSystemNotAddedException
 import org.example.logic.NoAuditsFoundedException
 import org.example.logic.entities.Audit
 import org.example.logic.repository.AuditRepository
 
 class AuditRepositoryImp(
-    private val auditSystemDataSource: AuditDataSource,
+    private val auditDataSource: AuditDataSource,
 ) : AuditRepository {
-    override suspend fun addAuditsEntries(audit: List<Audit>): Boolean =
-        executeSafelyWithContext(
-            onSuccess = {
-                auditSystemDataSource.addAuditsEntries(audit.map { it.mapToAuditModel() })
-            },
-            onFailure = { throw AuditSystemNotAddedException() }
-        )
+    override suspend fun addAuditsEntries(audit: List<Audit>): Boolean {
 
+        return tryToExecute({
+            auditDataSource.addAuditsEntries(audit.map { it.mapToAuditModel() })
+        }, onSuccess = { it }, onFailure = { throw AuditSystemNotAddedException() })
 
-    override suspend fun getAllAuditEntries(): List<Audit> =
-        executeSafelyWithContext(
-            onSuccess = {
-                auditSystemDataSource.getAllAuditEntries().mapNotNull { it.mapToAuditEntity() }
-            },
-            onFailure = { throw NoAuditsFoundedException() }
-        )
+    }
 
+    override suspend fun getAllAuditEntries(): List<Audit> {
+        return tryToExecute(
+            { auditDataSource.getAllAuditEntries().mapNotNull { it.mapToAuditEntity() } },
+            onSuccess = { it },
+            onFailure = { throw NoAuditsFoundedException() })
+    }
 
 }
