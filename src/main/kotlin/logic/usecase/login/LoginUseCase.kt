536e3e5502b395944fab176ba3edf955a5420aa4
Ahmed <ahmedsaaalah21@gmail.com>
2025-05-11 23:06:43 +0300

    removed tryToExecute fun from the logic layer

--- src/main/kotlin/logic/usecase/login/LoginUseCase.kt
+++ src/main/kotlin/logic/usecase/login/LoginUseCase.kt
@@ -2,7 +2,6 @@ package logic.usecase.login
 
 
 import logic.usecase.validation.ValidateUserDataUseCase
-import org.example.data.utils.tryToExecute
 import org.example.logic.IncorrectPasswordException
 import org.example.logic.UserDoesNotExistException
 import org.example.logic.UsersDataAreEmptyException
@@ -15,54 +14,47 @@ class LoginUseCase(
 ) {
     private var currentUser: User? = null
 
-    suspend fun loginUser(username: String, password: String): User = tryToExecute({
+    suspend fun loginUser(username: String, password: String): User = try {
         validateUserDataUseCase.validateUserName(username)
         validateUserDataUseCase.validatePassword(password)
         handleSuccess(
             username = username, password = password, users = userRepository.getAllUsers()
         )
-    }, onSuccess = { user ->
-        currentUser = user
-        user
-    }, onFailure = { throw UsersDataAreEmptyException() })
+    } catch (ex: Exception) {
+        throw UsersDataAreEmptyException()
+    }
 
 
-    private suspend fun handleSuccess(username: String, password: String, users: List<User>): User = tryToExecute(
-        {
-            val user = checkUserExists(users, username)
-            checkPassword(password, user)
-        },
-        onSuccess = { user -> user },
-        onFailure = { throw UsersDataAreEmptyException() },
-    )
+    private suspend fun handleSuccess(username: String, password: String, users: List<User>): User = try {
+        val user = checkUserExists(users, username)
+        checkPassword(password, user)
+    } catch (ex: Exception) {
+        throw UsersDataAreEmptyException()
+    }
 
 
-    private suspend fun checkUserExists(users: List<User>, username: String): User = tryToExecute(
-        {
-            users.find { user -> user.username == username }!!
-        },
-        onSuccess = { success -> success },
-        onFailure = { throw UserDoesNotExistException() }
-    )
+    private suspend fun checkUserExists(users: List<User>, username: String): User = try {
+        users.find { user -> user.username == username }!!
+    } catch (ex: Exception) {
+        throw UserDoesNotExistException()
+    }
 
 
-    private suspend fun checkPassword(password: String, user: User): User = tryToExecute(
-        {
-            if (hashToMd5(password) == user.hashedPassword) {
-                user
-            } else {
-                throw IncorrectPasswordException()
-            }
-        }, onSuccess = { user -> user },
-        onFailure = { throw IncorrectPasswordException() }
-    )
+    private suspend fun checkPassword(password: String, user: User): User = try {
+        if (hashToMd5(password) == user.hashedPassword) {
+            user
+        } else {
+            throw IncorrectPasswordException()
+        }
+    } catch (ex: Exception) {
+        throw IncorrectPasswordException()
+    }
 
-
-    suspend fun isUserExists(userName: String): Boolean = tryToExecute(
-        { userRepository.getAllUsers().any { it.username == userName } },
-        onSuccess = { success -> success },
-        onFailure = { false })
-
+    suspend fun isUserExists(userName: String): Boolean = try {
+        userRepository.getAllUsers().any { it.username == userName }
+    } catch (ex: Exception) {
+        false
+    }
 
     fun logout() {
         currentUser = null
