16363ae6cd54a80c866185a95466c26f9a236e07
Thoraya <thoriahamdy99@gmail.com>
2025-05-12 20:30:18 +0300

    implement csv data sources for all features

--- src/main/kotlin/data/source/local/TaskInProjectCSVDataSource.kt
+++ src/main/kotlin/data/source/local/TaskInProjectCSVDataSource.kt
@@ -2,21 +2,21 @@ package org.example.data.source.local
 
 import data.dto.TaskInProjectDto
 import org.example.data.source.TaskInProjectDataSource
+import org.example.data.source.local.csv_reader_writer.IReaderWriter
 
-class TaskInProjectCSVDataSource : TaskInProjectDataSource {
-    override suspend fun addTaskInProject(projectId: String, taskId: String): Boolean {
-        TODO("Not yet implemented")
-    }
+class TaskInProjectCSVDataSource(
+    private val taskInProjectReaderWriter: IReaderWriter<TaskInProjectDto>,
+) : TaskInProjectDataSource {
+    override suspend fun addTaskInProject(projectId: String, taskId: String): Boolean =
+        taskInProjectReaderWriter.append(listOf(TaskInProjectDto(projectId = projectId, taskId = taskId)))
 
-    override suspend fun deleteTaskFromProject(projectId: String, taskId: String): Boolean {
-        TODO("Not yet implemented")
-    }
+    override suspend fun deleteTaskFromProject(projectId: String, taskId: String): Boolean =
+        getAllTasksInProject().filterNot { taskInProject -> taskInProject.projectId == projectId && taskInProject.taskId == taskId }
+            .let { updatedTaskInProjectList -> taskInProjectReaderWriter.overWrite(updatedTaskInProjectList) }
 
-    override suspend fun getAllTasksInProject(): List<TaskInProjectDto> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getAllTasksInProject(): List<TaskInProjectDto> =
+        taskInProjectReaderWriter.read()
 
-    override suspend fun getTasksInProjectByProjectId(projectId: String): List<TaskInProjectDto> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getTasksInProjectByProjectId(projectId: String): List<TaskInProjectDto> =
+        getAllTasksInProject().filter { taskInProject -> taskInProject.projectId == projectId }
 }
--- src/main/kotlin/data/source/local/UserCSVDataSource.kt
+++ src/main/kotlin/data/source/local/UserCSVDataSource.kt
@@ -2,33 +2,34 @@ package org.example.data.source.local
 
 import data.dto.UserDto
 import org.example.data.source.UserDataSource
+import org.example.data.source.local.csv_reader_writer.IReaderWriter
 
-class UserCSVDataSource : UserDataSource {
-    override suspend fun addUser(user: UserDto): Boolean {
-        TODO("Not yet implemented")
-    }
+class UserCSVDataSource(
+    private val userReaderWriter: IReaderWriter<UserDto>,
+    private val userAssignedToProjectDataSource: UserAssignedToProjectCSVDataSource,
+) : UserDataSource {
+    override suspend fun addUser(user: UserDto): Boolean =
+        userReaderWriter.append(listOf(user))
 
-    override suspend fun getAllUsers(): List<UserDto> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getAllUsers(): List<UserDto> =
+        userReaderWriter.read()
 
-    override suspend fun getUsersByProjectId(projectId: String): List<UserDto> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getUsersByProjectId(projectId: String): List<UserDto> =
+        userAssignedToProjectDataSource.getUsersAssignedToProjectByProjectId(projectId)
+            .map { userAssignedToProject -> userAssignedToProject.userName }
+            .let { userIds -> getAllUsers().filter { user -> user.id in userIds } }
 
-    override suspend fun isUserExist(username: String): Boolean {
-        TODO("Not yet implemented")
-    }
+    override suspend fun isUserExist(username: String): Boolean =
+        getAllUsers().find { user -> user.username == username } != null
 
-    override suspend fun getUserById(userId: String): UserDto? {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getUserById(userId: String): UserDto? =
+        getAllUsers().find { user -> user.id == userId }
 
-    override suspend fun editUser(user: UserDto): Boolean {
-        TODO("Not yet implemented")
-    }
+    override suspend fun editUser(user: UserDto): Boolean =
+        getAllUsers().map { mappedUser -> if (mappedUser.id == user.id) user else mappedUser }
+            .let { updatedUsers -> userReaderWriter.overWrite(updatedUsers) }
 
-    override suspend fun deleteUser(user: UserDto): Boolean {
-        TODO("Not yet implemented")
-    }
+    override suspend fun deleteUser(user: UserDto): Boolean =
+        getAllUsers().filterNot { mappedUser -> mappedUser.id == user.id }
+            .let { updatedUsers -> userReaderWriter.overWrite(updatedUsers) }
 }
